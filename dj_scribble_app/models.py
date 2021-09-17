4c1b789907574a5cddf1a94cc1cc5d9a5caf10bf
ls <cancel>
2021-09-16 20:11:12 -0500

    CRUD for Post and Comment

--- dj_scribble_app/models.py
+++ dj_scribble_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.author
+
+class Comment(models.Model):
+    author = models.CharField(max_length=100)
+    body = models.CharField(max_length=200)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return self.author
