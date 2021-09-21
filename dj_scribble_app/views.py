d758a981fb6a1b2833bbadcd7d7f9eca2b83cf1c
ls <cancel>
2021-09-20 20:59:36 -0500

    post CRUB

--- dj_scribble_app/views.py
+++ dj_scribble_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 from .models import Comment, Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 # Create your views here.
 def post_list(request):
@@ -44,3 +44,34 @@ def post_delete(request, pk):
 def comment_list(request):
     comments = Comment.objects.all()
     return render(request, 'scribble/comment_list.html', {'comments': comments})
+
+def comment_detail(request, pk):
+    comment = Comment.objects.get(id=pk)
+    return render(request, 'scribble/comment_detail.html', {'comment': comment})
+
+def comment_create(request):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save()
+            return redirect('comment_detail', pk=comment.pk)
+        else:
+            form = CommentForm()
+    else:
+        form = CommentForm()
+    return render(request, 'scribble/comment_form.html', {'form': form})
+
+def comment_edit(request, pk):
+    comment = Post.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            comment = form.save()
+            return redirect('post_detail', pk=comment.pk)
+    else:
+        form=CommentForm(instance=comment)
+    return render(request, 'scribble/comment_form.html', {'form': form})
+
+def comment_delete(request, pk):
+    Comment.objects.get(id=pk).delete()
+    return redirect('comment_list')
